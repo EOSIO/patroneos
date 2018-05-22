7a82f5e2a84c54b03fdf96e7f52e8f9055ea8ad7
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2018-05-22 17:19:11 -0400

    Rename log mode to fail2ban-relay

--- docker/proxy/start.sh
+++ docker/proxy/start.sh
@@ -1,7 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
 fail2ban-client -b start
-patroneosd -configFile /etc/patroneos/config.json -mode log &
+patroneosd -configFile /etc/patroneos/config.json -mode fail2ban-relay &
 
 tail -f /var/log/patroneosd.log /var/log/fail2ban.log
